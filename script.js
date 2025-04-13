0202edddcca04af46175f535b9c8f60a78e94d1b
Abdulsammad55 <abdulsammad6366@gmail.com>
2025-04-13 07:50:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaBtn = document.querySelector('.cta-btn');
+    ctaBtn.addEventListener('click', () => {
+        alert("Welcome to the Battle Royale!");
+    });
+});
